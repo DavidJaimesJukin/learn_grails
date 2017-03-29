de0a2527fb679e87aac22476c8dcad0579adb1c2
David Jaimes <david.jaimes@jukinmedia.com>
2017-03-28 20:35:11 -0700

    Finished adding a small test to the app

--- myApp/src/test/groovy/com/example/ValueEstimateServiceSpec.groovy
+++ myApp/src/test/groovy/com/example/ValueEstimateServiceSpec.groovy
@@ -1,11 +1,13 @@
 package com.example
 
+import  grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
+@Mock([Make,Model, Vehicle])
 @TestFor(ValueEstimateService)
 class ValueEstimateServiceSpec extends Specification {
 
@@ -15,8 +17,19 @@ class ValueEstimateServiceSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "test estimate retrieval"(){
+        given: "a vehicle"
+        def make = new Make(name: "Test")
+        def model = new Model(name:"Test", make: make)
+        def vehicle = new Vehicle(year: 2000, make: make, model: model, name: "Test Vehiole")
+
+        when: "Service is called"
+        def estimate = service.getEstimate(vehicle)
+
+        then: "a non-zero result should be returned"
+        estimate > 0
+
+        and: "estimate is not to large"
+        estimate < 10000000
     }
 }
